f56211dae966999371dc5e94da736b28f953bda1
TANIGUCHI Makoto <master@h2o-space.com>
2025-04-18 20:45:57 +0900

    除外ファイルを更新

---
+++ public_html/wp-content/themes/forestcircularity/search.php
@@ -0,0 +1,30 @@
+<?php
+# キーワードを取得
+$keyword = get_search_query();
+# 検索件数
+$search_count = $wp_query->found_posts;
+?>
+
+<?php get_header(); ?>
+
+<div class="container--search">
+    <h2 class="search">「<?php echo esc_html($keyword); ?>」の検索結果</h2>
+    <p class="border--botom pb-none mb-none">検索結果<?php echo esc_html($search_count); ?>件</p>
+    <?php while (have_posts()) : the_post(); ?>
+    <div class="search--result">
+        <a href="<?php the_permalink(); ?>"></a>
+        <h3><?php the_title(); ?></h3>
+        <p><?php the_excerpt(); ?></p>
+    </div>
+    <?php endwhile; ?>
+
+    <?php the_posts_pagination([
+						'mid_size'      => 2, // 現在ページの左右に表示するページ番号の数
+						'prev_next'     => true, // 「前へ」「次へ」のリンクを表示する場合はtrue
+						'prev_text'     => __( '&lt;'), // 「前へ」リンクのテキスト
+						'next_text'     => __( '&gt;'), // 「次へ」リンクのテキスト
+						'type'          => 'list', // 戻り値の指定 (plain/list)
+					]); ?>
+</div>
+
+<?php get_footer(); ?>
